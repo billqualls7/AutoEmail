1f6ea8f2ce76091feba391f05e0375a8dba474b0
suntututut <wuyaosantu@qq.com>
2025-12-10 15:51:02 +0800

    1

--- backend/app/download.py
+++ backend/app/download.py
@@ -2,7 +2,7 @@
 Author: suntututut wuyaosantu@qq.com
 Date: 2025-12-10 15:32:02
 LastEditors: suntututut wuyaosantu@qq.com
-LastEditTime: 2025-12-10 15:48:16
+LastEditTime: 2025-12-10 15:48:29
 FilePath: /AutoEmail/backend/app/download.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
